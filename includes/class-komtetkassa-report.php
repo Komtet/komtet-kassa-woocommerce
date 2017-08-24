1f74c96157271b979f5285ac6b627222f37bbfb9
potalius <potalius@gmail.com>
2017-08-24 08:28:19 +0300

    Issue #3: Добавил под контроль версий class-komtetkassa-report.php

---
+++ includes/class-komtetkassa-report.php
@@ -0,0 +1,14 @@
+<?php
+
+final class KomtetKassa_Report {
+
+    public function create($order_id, $data, $error="")
+    {
+
+    }
+
+    public function update($order_id, $state, $data)
+    {
+
+    }
+}
